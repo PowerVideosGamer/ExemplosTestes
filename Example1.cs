d6129156d57a51c976b6a69f6ebd6a01c1c1d369
PowerVideosGamer <126355963+PowerVideosGamer@users.noreply.github.com>
2025-03-05 15:13:02 -0300

    Add files via upload

---
+++ Example1.cs
@@ -0,0 +1,6 @@
+// OBS: A programação C# é orientada a objetos.
+// Exemplo de biblioteca de Sistema.
+
+using System; 
+// Está biblioteca indica que eu estou importando para o meu projeto as funções "System".
+
